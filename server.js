84b2197bd52b8994faf5a9ae0554e41c6f042860
19121A04K4 <86012770+19121A04K4@users.noreply.github.com>
2025-07-29 23:43:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
